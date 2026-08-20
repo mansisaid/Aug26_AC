f7ac9f2e44d693467b3f016219e9fc3d110e0075
Mansi Suresh Said <mansisaid544@gmail.com>
2026-08-20 18:05:44 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,18 @@
+//Write a Java program that checks if a predefined number is odd or even. Use an if-else statement and the modulus operator (%) to determine whether the number is divisible by 2 or not. 
+class CheckEvenOdd{
+	
+	public static void main(String args[]){
+
+	int number = 15;
+
+	if(number % 2 == 0){
+			System.out.println(number + " is Even Number");
+	}
+	else{
+		System.out.println(number + " is Odd Number");
+	}
+
+
+	}
+
+}
