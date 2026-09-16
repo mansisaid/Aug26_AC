165fafb2325c2c289b217fece72785fbc0c79bd8
Mansi Suresh Said <mansisaid544@gmail.com>
2026-09-17 00:10:51 +0530

    Add files via upload

---
+++ OOPJ Module/Assignments/EvenNumbers.java
@@ -0,0 +1,14 @@
+public class EvenNumbers 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=500;i++)
+		{
+			if(i %2 == 0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
